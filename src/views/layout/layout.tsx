cfa3ec1ed74e38b617ef028e8910bcb869645e72
Kyle Joseph Timajo <timajokyle24@gmail.com>
2024-04-02 00:44:41 +0800

    feat: add layout

---
+++ src/views/layout/layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from '@/components/navbar';
+import React from 'react';
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <div className="">
+            <Navbar />
+            <div className="px-[30px] sm:px-[64px] w-full text-primary-text">
+                <div className="w-full max-w-[1112px] mx-auto">{children}</div>
+            </div>
+        </div>
+    );
+}
