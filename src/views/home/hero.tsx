a8c6c1b071ce5f7399fe5eaecbcc930774ac48d2
Kyle Joseph Timajo <timajokyle24@gmail.com>
2024-04-02 00:45:48 +0800

    feat: homepage ui

---
+++ src/views/home/hero.tsx
@@ -0,0 +1,24 @@
+import Button from '@/components/button';
+import React from 'react';
+
+export default function HeroSection() {
+    return (
+        <div className="flex flex-col-reverse 900px:flex-row items-center justify-between gap-1">
+            <div id="heading" className="space-y-[25px] max-w-[500px]">
+                <div className="space-y-[15px]">
+                    <h1 className="font-europa text-4xl sm:text-5xl text-primary">
+                        Hello, I’m Redeemer: <br /> A UI/UX Designer
+                    </h1>
+                    <p className="text-lg sm:text-xl">
+                        I'm a passionate UI/UX designer driven by creating intuitive and user-friendly experiences. I
+                        specialized in clean and minimalist designs.
+                    </p>
+                </div>
+                <Button>Let's chat</Button>
+            </div>
+            <div id="hero-image" className="h-full shrink-0">
+                <img src="/assets/images/hero-image.png" alt="Redeemer Olaivar" className="h-full" />
+            </div>
+        </div>
+    );
+}
---
+++ src/views/home/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeroSection from './hero';
+
+export default function HomePage() {
+    return (
+        <div className="w-full">
+            <HeroSection />
+        </div>
+    );
+}
