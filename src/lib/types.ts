980de7e96c8fa11a899fb80eae08f6410750347e
Kyle Joseph Timajo <timajokyle24@gmail.com>
2024-04-03 00:58:49 +0800

    feat: add reusable types

---
+++ src/lib/types.ts
@@ -0,0 +1,14 @@
+export interface IWorkCard {
+    title: string;
+    description: string;
+    image: string;
+    image2?: string;
+    url?: string;
+    bgClass: string;
+    shadowClass: string;
+    titleClass: string;
+    buttonClass: string;
+    descriptionClass: string;
+    reversed?: boolean;
+    alternate?: boolean;
+}
