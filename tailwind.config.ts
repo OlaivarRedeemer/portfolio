5e393d7b85d1c0d9e820ae2aa9c3854e6add5ad1
Kyle Joseph Timajo <timajokyle24@gmail.com>
2025-01-09 23:19:31 +0800

    chore: update color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,51 +1,51 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config = {
-    darkMode: ['class'],
-    content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
-    prefix: '',
-    theme: {
-        container: {
-            center: true,
-            padding: '2rem',
-            screens: {
-                '2xl': '1400px',
-            },
-        },
-        extend: {
-            keyframes: {
-                'accordion-down': {
-                    from: { height: '0' },
-                    to: { height: 'var(--radix-accordion-content-height)' },
-                },
-                'accordion-up': {
-                    from: { height: 'var(--radix-accordion-content-height)' },
-                    to: { height: '0' },
-                },
-            },
-            animation: {
-                'accordion-down': 'accordion-down 0.2s ease-out',
-                'accordion-up': 'accordion-up 0.2s ease-out',
-            },
-            colors: {
-                primary: '#333333',
-                secondary: '#F4F4F4',
-                'primary-text': '#71717A',
-                'secondary-text': '#E8E8E8',
-            },
-            fontFamily: {
-                europa: ['Europa'],
-                roboto: ['Roboto'],
-            },
-            screens: {
-                '900px': '900px',
-                '1000px': '1000px',
-                '1100px': '1100px',
-                '1200px': '1200px',
-            },
-        },
+  darkMode: ["class"],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    plugins: [require('tailwindcss-animate')],
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
+        primary: "#080808",
+        secondary: "#E60E37",
+        tertiary: "#3C3C3C",
+        "primary-text": "#71717A",
+        "secondary-text": "#E8E8E8",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      screens: {
+        "900px": "900px",
+        "1000px": "1000px",
+        "1100px": "1100px",
+        "1200px": "1200px",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
