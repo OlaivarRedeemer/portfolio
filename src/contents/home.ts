a04fe283a6997ab35841b80f954f94242edd8875
Kyle Joseph Timajo <timajokyle24@gmail.com>
2025-01-13 22:55:51 +0800

    feat: add projects section content
    Signed-off-by: Kyle Joseph Timajo <timajokyle24@gmail.com>

--- src/contents/home.ts
+++ src/contents/home.ts
@@ -1,28 +1,40 @@
-import { IWorkCard } from '@/lib/types';
+import { IProject } from "@/lib/types";
 
-export const workSectionContent: IWorkCard[] = [
-    {
-        title: 'Brooky',
-        description:
-            'Designing the system of a startup that automates the reservation process for real estate transactions.',
-        image: '/assets/images/works/brooky-dashboard-1.png',
-        bgClass: 'bg-[#102F52] rounded-none sm:rounded-3xl',
-        titleClass: 'text-white',
-        descriptionClass: 'text-secondary-text',
-        buttonClass: 'text-white',
-        shadowClass: 'shadow-[0px_4px_50px_0px#071C34]',
-        url: '/',
-    },
-    {
-        title: 'Dazle',
-        description: 'Designing the website that organized property listings for real estate professionals.',
-        image: '/assets/images/works/dazle-1.png',
-        bgClass: 'bg-[#1F4BC6] rounded-none sm:rounded-3xl',
-        titleClass: 'text-white',
-        descriptionClass: 'text-secondary-text',
-        buttonClass: 'text-white',
-        shadowClass: 'shadow-[0px_4px_50px_0px#4140A8]',
-        url: '/',
-        reversed: true,
-    },
+export const projects: IProject[] = [
+  {
+    image: "/assets/projects/brooky.png",
+    title: "Brooky",
+    description: "UI design, Web (Responsive) Design",
+    detailSlug: "/",
+  },
+  {
+    image: "/assets/projects/dazle.png",
+    title: "Dazle",
+    description: "UI design, Web (Responsive) Design",
+    detailSlug: "/",
+  },
+  {
+    image: "/assets/projects/venue4use.png",
+    title: "Venue4Use",
+    description: "UI design, Mobile App",
+    detailSlug: "/",
+  },
+  {
+    image: "/assets/projects/plateful.png",
+    title: "Plateful",
+    description: "UI design, Mobile App",
+    detailSlug: "/",
+  },
+  {
+    image: "/assets/projects/nexus.png",
+    title: "Nexus",
+    description: "Landing Page",
+    detailSlug: "/",
+  },
+  {
+    image: "/assets/projects/amplify.png",
+    title: "Amplify",
+    description: "Landing Page",
+    detailSlug: "/",
+  },
 ];
