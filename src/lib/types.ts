a4119ac337c1f1f36992d140b0f5e2e3083286b7
Kyle Joseph Timajo <timajokyle24@gmail.com>
2025-01-13 22:55:32 +0800

    feat: add new type
    Signed-off-by: Kyle Joseph Timajo <timajokyle24@gmail.com>

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,14 +1,6 @@
-export interface IWorkCard {
-    title: string;
-    description: string;
-    image: string;
-    image2?: string;
-    url?: string;
-    bgClass: string;
-    shadowClass?: string;
-    titleClass: string;
-    buttonClass: string;
-    descriptionClass: string;
-    reversed?: boolean;
-    alternate?: boolean;
+export interface IProject {
+  image: string;
+  title: string;
+  description: string;
+  detailSlug: string;
 }
