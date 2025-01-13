ab0964ac51b215e487d5f940ac559ce92478c6eb
Kyle Joseph Timajo <timajokyle24@gmail.com>
2025-01-13 23:04:23 +0800

    fix: lint
    Signed-off-by: Kyle Joseph Timajo <timajokyle24@gmail.com>

--- src/components/home/contact-section.tsx
+++ src/components/home/contact-section.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function ContactSection() {
   return (
     <div className="py-[190px] space-y-[30px]">
-      <h1 className="text-center text-4xl font-extrabold">Let's work together</h1>
+      <h1 className="text-center text-4xl font-extrabold">{"Let's work together"}</h1>
       <div className="flex justify-center items-center">
         <Link href="mailto:redeemerolaivar03@gmail.com">
           <Button variant="secondary" className="h-[67px] w-[138px] text-lg flex items-center gap-1.5">
