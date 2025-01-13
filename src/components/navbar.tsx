a734dac6b9ba6877448fd62ba9f4267c37bb327d
Kyle Joseph Timajo <timajokyle24@gmail.com>
2025-01-13 23:28:06 +0800

    feat: apply glassmorphism
    Signed-off-by: Kyle Joseph Timajo <timajokyle24@gmail.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
-    <div className="sticky z-[100] top-0 left-0 w-full flex items-center justify-between h-[80px] px-[30px] sm:px-10 py-2 bg-primary">
+    <div className="sticky z-[100] top-0 left-0 w-full flex items-center justify-between h-[70px] px-[30px] sm:px-10 py-2 bg-white-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
       <div id="logo">
         <img src="/assets/icons/logo.svg" alt="logo" />
       </div>
