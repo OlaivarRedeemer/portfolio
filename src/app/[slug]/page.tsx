12039206ce96afdd64f48773bebdc9bcc2b00b7d
Kyle Joseph Timajo <timajokyle24@gmail.com>
2025-01-15 02:00:55 +0800

    chore: minor updates

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,21 @@
 import { projectDetails } from "@/contents/project-detail";
 import ProjectDetailPage from "@/views/project-detail/project-detail";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Redeemer Olaivar",
+  description:
+    "I'm a passionate UI/UX designer driven by creating intuitive and user-friendly experiences. I specialized in clean and minimalist designs.",
+  icons: {
+    icon: [
+      {
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+    ],
+  },
+};
 
 export const generateStaticParams = async () => {
   const paths = ["/brooky", "/dazle", "/venue4use", "/plateful", "/nexus", "/amplify"];
